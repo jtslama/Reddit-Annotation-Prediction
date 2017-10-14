a6bba506bd4176d451416eadf07ecd07a59d8dcf
John Slama <jtslama@sbcglobal.net>
2017-10-13 18:52:25 -0700

    basic scraper structure

--- initial_data_loading.py
+++ initial_data_loading.py
@@ -3,6 +3,7 @@ import json
 import ast
 import requests
 from bs4 import BeautifulSoup
+import time
 
 
 def load_discourse_data(filepath):
@@ -37,40 +38,85 @@ If I want to download only a small subset of the reddit data (for instance,
 dataframe to find the date.
 """
 
-class date_finder(object):
+class Reddit_Post_Date_Finder(object):
     """
     To solve a problem I didn't know I had
+    TODO: datetime handling, what goes in init?
     """
-    def __init__(self, url_list):
-        self.url_list = urls
+    def __init__(self):
+        pass
 
-    def query_site(self, url):
+    def _query_site(self, url):
+        """
+        Retrieves the html content from a single url
+        Exits if it receives an invalid code, with the code received
+        INPUTS:
+        url (string) - url to visit
+        OUTPUTS:
+        res.text (string) - the html content from the url
+        """
         res = requests.get(url)
-        if not res.status_code == res.codes.ok:
-            print "WARNING: status code ", res.status_code
+        if not res.status_code == requests.codes.ok:
+            print("WARNING: status code", res.status_code)
+            print('\n')
         else:
+            print("Loading html from: ")
+            print(url)
             return res.text
 
-    def parse_html(self, html_str):
+    def _parse_html(self, html_str):
+        """
+        Parses html to find the date of the post
+        INPUTS:
+        html_str (string) - html from a page
+        OUTPUTS:
+        date -
+        """
+        #TODO finish documentation when you figure out date type
         soup = BeautifulSoup(html_str, 'html.parser')
         date = soup.select("p[class='tagline'] > time")
+        #TODO: need to further refine date to just day
+        return date
+
+    def get_dates(self, url_list, time_delay=5):
+        """
+        Runs _query_site and _parse_html for every url in url_list, returning
+        the dates in the order of the urls.
+        INPUTS:
+        url_list (list of strings) - urls to scrape
+        time_delay (int) - how long to wait in between urls (in seconds). Defaults to 5 seconds.
+        OUTPUTS:
+        dates (list) - dates, in the order of url_list
+        """
+        #TODO need to make sure the wait time is correct before using
+        #in case url is a single string
+        if type(url_list) is str:
+            url_list = [url_list]
+        if type(url_list) not list:
+            print("This is a {} and needs to be a list:\n{}").format(type(url_list), url_list)
+        # go through the url list, get the html from the url, and find the date
+        dates = []
+        for i, url in enumerate(url_list):
+            html_text = _query_site(url)
+            print("{}/{}/n").format(i, len(url_list))            
+            date = _parse_html(html_text)
+            #TODO need to further refine date (Either here or in above fn)
+            dates.append(date)
+            #be sure to wait before repeating to avoid getting banned
+            time.sleep(time_delay)
+
+        return dates
+
+
 
 
 
 if __name__ == '__main__':
     orig_json_file = 'coarse-discourse/coarse_discourse_dataset.json'
     df = load_discourse_data(orig_json_file)
-    df.url[0]
-    df.posts[0]
-    df.head(1)
+    df.head()
     len(df)
-
-    res = requests.get(url)
-    if not res.status_code == res.codes.ok:
-        print "WARNING: status code ", res.status_code
-    else:
-        return res.text
-
-
-    soup = BeautifulSoup(html_str, 'html.parser')
-    date = soup.select("p[class='tagline'] > time")
+    # small test case (to test scraper)
+    urls = df.url.tolist()[:10]
+    Finder = Reddit_Post_Date_Finder()
+    dates = Finder.get_dates(urls, time_delay=10)
